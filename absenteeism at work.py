2f45acd34f02419bd08a4fa5dc3b1dba4f9b5863
Nachiketa-Kumar <60153793+Nachiketa-Kumar@users.noreply.github.com>
2020-01-22 01:19:11 +0530

    Delete absenteeism at work.py

--- absenteeism at work.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-df=pd.read_csv('C:/Users/Nachiketa/Desktop/ml1/absent_uciv_ass2/Absenteeism_at_work.csv', delimiter=";")
-df
-df.head(10)
