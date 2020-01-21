7b07d05cb41a9430db68af2dbcf6655fdc952c94
Nachiketa-Kumar <60153793+Nachiketa-Kumar@users.noreply.github.com>
2020-01-22 00:51:25 +0530

    Add files via upload
    The data set was on absenteeism  at work and most appropriate algorithms are used in order to give the optimal solutions.

---
+++ absenteeism at work.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+df=pd.read_csv('C:/Users/Nachiketa/Desktop/ml1/absent_uciv_ass2/Absenteeism_at_work.csv', delimiter=";")
+df
+df.head(10)
